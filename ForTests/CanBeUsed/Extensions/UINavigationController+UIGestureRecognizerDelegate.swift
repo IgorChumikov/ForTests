b386385cd6a8b1ef1f1b5f1dbd26f7ced541d08b
Igor <chumikov.igor@mail.ru>
2024-03-06 22:36:42 +0700

    add UINavigationController

---
+++ ForTests/CanBeUsed/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  UINavigationController+UIGestureRecognizerDelegate.swift
+//  ForTests
+//
+//  Created by Игорь Чумиков on 06.03.2024.
+//
+
+import UIKit
+
+private enum Constants {
+    static let minimumViewControllerCount: Int = 1
+}
+
+// Add the ability to tap with a finger to go back to the previous screen throughout the application
+// if the number of controllers in the stack is greater than the specified minimum number (in this case, 1)
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > Constants.minimumViewControllerCount
+    }
+}

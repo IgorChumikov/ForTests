fa0d9fbd5d381736910f246807cc36f83ca7daa6
Igor <chumikov.igor@mail.ru>
2024-05-15 19:45:33 +0700

    ааа

---
+++ ForTests/ForTests/Work/ResizableSheets2View.swift
@@ -0,0 +1,18 @@
+//
+//  ResizableSheets2View.swift
+//  ForTests
+//
+//  Created by Игорь Чумиков on 15.05.2024.
+//
+
+import SwiftUI
+
+struct ResizableSheets2View: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ResizableSheets2View()
+}
---
+++ ForTests/ForTests/Work/ResizableSheetsView.swift
@@ -0,0 +1,30 @@
+//
+//  ResizableSheetsView.swift
+//  ForTests
+//
+//  Created by Игорь Чумиков on 15.05.2024.
+//
+
+import SwiftUI
+
+struct ResizableSheetsView: View {
+    
+    
+    @State var showSweet: Bool = false
+    
+    var body: some View {
+        Button("Click mi") {
+            showSweet.toggle()
+        }
+        .sheet(isPresented: $showSweet) {
+            ResizableSheets2View()
+                .presentationDetents([.height(200)])
+                .presentationDragIndicator(.hidden)
+                .interactiveDismissDisabled()
+        }
+    }
+}
+
+#Preview {
+    ResizableSheetsView()
+}

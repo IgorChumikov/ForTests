a1c2da2b3f03e8ac53611f99270570b4371efe0f
Igor <chumikov.igor@mail.ru>
2024-05-15 17:55:45 +0700

    f

--- ForTests/ForTests/Work/New/MainCurtainView.swift
+++ ForTests/ForTests/Work/New/MainCurtainView.swift
@@ -26,3 +26,39 @@ struct MainCurtainView: View {
 #Preview {
     MainCurtainView()
 }
+
+
+
+
+//struct MainCurtainView: View {
+//    
+//    @State private var isSheetPresented: Bool = false
+//    
+//    var body: some View {
+//        ZStack {
+//            content
+//            if isSheetPresented {
+//                Color.black.opacity(0.4)
+//                    .edgesIgnoringSafeArea(.all)
+//                    .onTapGesture {
+//                        isSheetPresented.toggle()
+//                    }
+//                
+//                ExitCurtainView(isSheetPresented: $isSheetPresented)
+//                    .transition(.move(edge: .bottom))
+//                    .zIndex(1)
+//            }
+//        }
+//        .animation(.easeInOut(duration: 0.3))
+//    }
+//    
+//    private var content: some View {
+//        VStack {
+//            Button("До 16 IOS") {
+//                isSheetPresented.toggle()
+//            }
+//        }
+//        .disabled(isSheetPresented)  // Отключаем взаимодействие, когда лист показан
+//        .blur(radius: isSheetPresented ? 3 : 0)  // Добавляем размытие
+//    }
+//}

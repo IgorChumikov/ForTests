afeb67f1475e281962d11a84bae38e5d7fd6cf04
Igor <chumikov.igor@mail.ru>
2024-06-13 14:33:13 +0300

    dddd

--- ForTests/ForTests/Work/Tran/View5.swift
+++ ForTests/ForTests/Work/Tran/View5.swift
@@ -7,8 +7,12 @@
 
 import SwiftUI
 
+enum ActiveView {
+    case main, view1, view2, view3, view4
+}
+
 class NavigationStackManager: ObservableObject {
-    @Published var activeView: Int = 0
+    @Published var activeView: ActiveView = .main
 }
 
 struct View5: View {
@@ -18,21 +22,19 @@ struct View5: View {
         NavigationView {
             VStack {
                 switch navigationStackManager.activeView {
-                case 0:
+                case .main:
                     Button("Переход на View5_1") {
-                        navigationStackManager.activeView = 1
+                        navigationStackManager.activeView = .view1
                     }
                     .navigationTitle("View5")
-                case 1:
+                case .view1:
                     View5_1()
-                case 2:
+                case .view2:
                     View5_2()
-                case 3:
+                case .view3:
                     View5_3()
-                case 4:
+                case .view4:
                     View5_4()
-                default:
-                    EmptyView()
                 }
             }
         }
@@ -52,12 +54,12 @@ struct View5_1: View {
             Color.red.ignoresSafeArea()
             VStack {
                 Button("Переход на View5_2") {
-                    navigationStackManager.activeView = 2
+                    navigationStackManager.activeView = .view2
                 }
             }
             .navigationTitle("View5_1")
             .onAppear {
-                navigationStackManager.activeView = 1
+                navigationStackManager.activeView = .view1
             }
         }
     }
@@ -71,12 +73,12 @@ struct View5_2: View {
             Color.brown.ignoresSafeArea()
             VStack {
                 Button("Переход на View5_3") {
-                    navigationStackManager.activeView = 3
+                    navigationStackManager.activeView = .view3
                 }
             }
             .navigationTitle("View5_2")
             .onAppear {
-                navigationStackManager.activeView = 2
+                navigationStackManager.activeView = .view2
             }
         }
     }
@@ -90,12 +92,12 @@ struct View5_3: View {
             Color.yellow.ignoresSafeArea()
             VStack {
                 Button("Переход на View5_4") {
-                    navigationStackManager.activeView = 4
+                    navigationStackManager.activeView = .view4
                 }
             }
             .navigationTitle("View5_3")
             .onAppear {
-                navigationStackManager.activeView = 3
+                navigationStackManager.activeView = .view3
             }
         }
     }
@@ -110,12 +112,12 @@ struct View5_4: View {
             VStack {
                 Text("Это View5_4")
                 Button("На главный экран") {
-                    navigationStackManager.activeView = 0
+                    navigationStackManager.activeView = .main
                 }
             }
             .navigationTitle("View5_4")
             .onAppear {
-                navigationStackManager.activeView = 4
+                navigationStackManager.activeView = .view4
             }
         }
     }

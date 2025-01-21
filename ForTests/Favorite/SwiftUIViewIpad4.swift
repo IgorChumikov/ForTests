6495114f9aad48c06380610961e4c469c2322c27
Igor <chumikov.igor@mail.ru>
2025-01-21 15:40:21 +0700

    add

--- ForTests/Favorite/SwiftUIViewIpad4.swift
+++ ForTests/Favorite/SwiftUIViewIpad4.swift
@@ -8,21 +8,52 @@
 import SwiftUI
 
 struct SwiftUIViewIpad4: View {
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(0..<10) { index in
-                    NavigationLink("Item \(index)", destination: Text("Detail \(index)"))
+                Section {
+                    NavigationLink(destination: Text("Мой экран 1")) {
+                        HStack(alignment: .center, spacing: 10) {
+                            Image(systemName: "bookmark") // Иконка перед текстом
+                                .foregroundColor(.blue) // Цвет иконки
+                            Text("Закладки и документы") // Текст строки
+                        }
+                    }
+                    
+                    NavigationLink(destination: Text("Мой экран 2")) {
+                        HStack(alignment: .center, spacing: 10) {
+                            Image(systemName: "folder") // Иконка перед текстом
+                                .foregroundColor(.orange)
+                            Text("Папки")
+                        }
+                    }
+                    
+                    NavigationLink(destination: Text("Мой экран 3")) {
+                        HStack(alignment: .center, spacing: 10) {
+                            Image(systemName: "doc.text") // Иконка перед текстом
+                                .foregroundColor(.green)
+                            Text("Документы на контроле")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Мой экран 4")) {
+                        HStack {
+                            Image(systemName: "tray.and.arrow.down") // Иконка перед текстом
+                                .foregroundColor(.purple)
+                            Text("Загруженные документы")
+                        }
+                    }
                 }
             }
-            .listStyle(SidebarListStyle())
-            .navigationTitle("Menu")
-            
-            Text("Select an item")
+            .listStyle(SidebarListStyle()) // Стиль бокового меню
+            .scrollDisabled(true) // Отключение прокрутки
         }
-        .navigationViewStyle(DoubleColumnNavigationViewStyle())
     }
 }
+
 
 #Preview {
     SwiftUIViewIpad4()

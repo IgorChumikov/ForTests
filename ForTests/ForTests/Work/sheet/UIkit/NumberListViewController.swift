60123b028fa8da226969758cb58a2d29d3b8f680
Igor <chumikov.igor@mail.ru>
2025-05-15 20:46:20 +0700

    фвв

--- ForTests/ForTests/Work/sheet/UIkit/NumberListViewController.swift
+++ ForTests/ForTests/Work/sheet/UIkit/NumberListViewController.swift
@@ -5,23 +5,38 @@
 //  Created by Игорь Чумиков on 15.05.2025.
 //
 
+
 import SwiftUI
 
 class NumberListViewController: UIViewController, UITableViewDataSource {
     private let tableView = UITableView()
     private let numbers = Array(1...100)
+    private let buttonHostingController = UIHostingController(rootView: SwiftUIViewTestSheet())
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Список чисел"
 
+        // Add SwiftUI button view
+        addChild(buttonHostingController)
+        view.addSubview(buttonHostingController.view)
+        buttonHostingController.didMove(toParent: self)
+        buttonHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        // Add TableView
         tableView.dataSource = self
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
+        // Layout
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            buttonHostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            buttonHostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            buttonHostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            buttonHostingController.view.heightAnchor.constraint(equalToConstant: 60),
+
+            tableView.topAnchor.constraint(equalTo: buttonHostingController.view.bottomAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -38,6 +53,7 @@ class NumberListViewController: UIViewController, UITableViewDataSource {
         return cell
     }
 }
+
 
 
 // MARK: - SwiftUI Preview
